36fd9d5a11dddd2904a778202c9de22adce28080
bennitalg <bennitalg@outlook.com>
2018-09-12 13:27:47 -0500

    MySentence

--- CG 6-2/Program.cs
+++ CG 6-2/Program.cs
@@ -6,7 +6,19 @@ namespace CG_6_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Create a program that converts a normal sentence input by the user into 
+            all upper case and prints the value to the console.
+            */
+
+            string mySentence = "I am a kickass coder girl ";
+            
+            mySentence = mySentence.ToUpper();
+            Console.WriteLine(mySentence);
+            Console.ReadLine();
+
+            
+            
         }
+         
     }
 }
